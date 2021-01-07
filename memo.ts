68f3d54e3eb354d2158a0b45dde2e528988ec906
devgony <devgony@gmail.com>
2021-01-08 00:20:06 +0900

    createBlock

--- memo.ts
+++ memo.ts
@@ -6,3 +6,25 @@
 const sayHi = (name: string, age: number, gender?: string): string => {
   return `Hello ${name}, you are ${age}, you are a ${gender}`;
 };
+// 4. interface: define the object type on TS level
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+const nate = { name: "Nate", age: 29, gender: "M" };
+// 5. class: implicitly define object type on JS level (privacy work on TS only)
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+const henry = new Human("Henry", 29, "M");
+// 6. type in array
+let blockchain: Block[] = [genesisBlock];
+// 7. static function is callable, even if class is not instantiated
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,57 @@
-const name = "henry",
-  age = 29,
-  gender = "M";
+import * as CryptoJS from "crypto-js";
 
-const sayHi = (name: string, age: number, gender?: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  // static function is callable, even if class is not instantiated
+  static calculateBlockHash = (
+    index: number,
+    previsouHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previsouHash + timestamp + data).toString();
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+const genesisBlock: Block = new Block(0, "412341453214", "", "Hello", 123456);
+let blockchain: Block[] = [genesisBlock];
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimeStmp = (): number => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const nextTimestamp: number = getNewTimeStmp();
+  const nextHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    nextTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    nextHash,
+    previousBlock.hash,
+    data,
+    nextTimestamp
+  );
+  return newBlock;
 };
-console.log(sayHi(name, age, gender));
 
+console.log(createNewBlock("hello"), createNewBlock("byebye"));
 export {};
