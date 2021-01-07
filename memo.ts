36c494fd6bc856ca29a8bf2dc81aa00cd6a5249b
devgony <devgony@gmail.com>
2021-01-07 23:03:52 +0900

    init2

--- memo.ts
+++ memo.ts
@@ -1,6 +1,8 @@
 // memo.ts;
 // 0. export {}; at the bottom
-// 1. send proper number of arguments unless define parameter with "?"
-const sayHi = (name, age, gender?) => {};
-// 2. use type with params
-const sayHi2 = (name: string, age: number, gender: string) => {};
+// 1. send proper number of arguments unless define params with "?"
+// 2. use type after params
+// 3. return type like string, void
+const sayHi = (name: string, age: number, gender?: string): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
