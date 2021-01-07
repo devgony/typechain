83d01cb788f3d7435794daf489bbb1bc2617932c
devgony <devgony@gmail.com>
2021-01-07 23:05:11 +0900

    init3

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "henry", age = 29, gender = "M";
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
-};
-console.log(sayHi(name, age, gender));
-//# sourceMappingURL=index.js.map
